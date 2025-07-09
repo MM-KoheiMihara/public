55c37b22263ec9ce66a4ebbf44fc4ec88b7ff67e
MM-KoheiMihara <ko-mihara@medicmedia.com>
2025-07-09 18:01:46 +0900

    Update 20250709_slider.js

--- 20250709_slider.js
+++ 20250709_slider.js
@@ -1,152 +1,47 @@
-    /* ------------ 1) 画像 URL を列挙 ------------ */
-    const IMAGE_LIST = [
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg000.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg001.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg002.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg003.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg004.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg005.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg006.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg007.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg008.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg009.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg010.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg011.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg012.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg013.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg014.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg015.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg016.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg017.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg018.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg019.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg020.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg021.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg022.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg023.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg024.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg025.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg026.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg027.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg028.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg029.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg030.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg031.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg032.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg033.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg034.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg035.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg036.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg037.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg038.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg039.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg040.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg041.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg042.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg043.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg044.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg045.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg046.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg047.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg048.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg049.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg050.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg051.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg052.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg053.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg054.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg055.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg056.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg057.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg058.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg059.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg060.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg061.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg062.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg063.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg064.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg065.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg066.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg067.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg068.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg069.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg070.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg071.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg072.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg073.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg074.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg075.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg076.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg077.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg078.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg079.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg080.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg081.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg082.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg083.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg084.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg085.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg086.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg087.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg088.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg089.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg090.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg091.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg092.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg093.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg094.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg095.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg096.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg097.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg098.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg099.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg100.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg101.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg102.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg103.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg104.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg105.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg106.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg107.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg108.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg109.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg110.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg111.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg112.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg113.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg114.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg115.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg116.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg117.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg118.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg119.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg120.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg121.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg122.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg123.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg124.jpg',
-　'https://medilink-contents-qb-stg.s3.ap-northeast-1.amazonaws.com/9B00000/images/CTimg125.jpg'
-    ];
-    /* 実際の URL に置き換えてください */
+/* =======================================================
+ *  汎用 画像スタック・スライダー
+ *    - 画像 URL は <div id="viewer" data-images="url1,url2, ..."> に列挙
+ *    - ビューア:   <img id="ctImg">
+ *    - スライダー: <input type="range" id="sliceRange">
+ *    - カウンタ:   <span id="counter">
+ * ======================================================= */
+(() => {
+  /* ---------- 1) 画像リスト取得 ---------- */
+  const viewer = document.getElementById('viewer');
+  if (!viewer) { console.error('viewer 要素が見つかりません。'); return; }
 
-    /* ------------ 2) 要素取得と初期設定 ------------ */
-    const img   = document.getElementById('ctImg');
-    const range = document.getElementById('sliceRange');
-    const cnt   = document.getElementById('counter');
+  const rawList = viewer.dataset.images || '';
+  const IMAGE_LIST = rawList.split(',').map(s => s.trim()).filter(Boolean);
 
-    range.max       = IMAGE_LIST.length - 1;   // 0〜9
-    img.src         = IMAGE_LIST[0];           // 最初の画像
-    cnt.textContent = `1 / ${IMAGE_LIST.length}`;
+  if (IMAGE_LIST.length === 0) {
+    console.error('data-images が空、または画像 URL が見つかりません。');
+    return;
+  }
 
-    /* ------------ 3) スライダーで切り替え ------------ */
-    range.addEventListener('input', e => {
-      const idx = Number(e.target.value);      // 0〜9
-      img.src   = IMAGE_LIST[idx];
-      cnt.textContent = `${idx + 1} / ${IMAGE_LIST.length}`;
-    });
+  /* ---------- 2) 要素参照 ---------- */
+  const img   = document.getElementById('ctImg');
+  const range = document.getElementById('sliceRange');
+  const cnt   = document.getElementById('counter');
 
-    /* ------------ 4) プリロード（任意） ------------ */
-    (function preload(){
-      const buf = new Image();
-      for(const src of IMAGE_LIST){ buf.src = src; }
-    })();
+  if (!img || !range || !cnt) {
+    console.error('ctImg / sliceRange / counter のいずれかが見つかりません。');
+    return;
+  }
+
+  /* ---------- 3) 初期化 ---------- */
+  range.min = 0;
+  range.max = IMAGE_LIST.length - 1;
+  range.value = 0;
+  img.src = IMAGE_LIST[0];
+  cnt.textContent = `1 / ${IMAGE_LIST.length}`;
+
+  /* ---------- 4) スライダー操作 ---------- */
+  range.addEventListener('input', e => {
+    const idx = Number(e.target.value);
+    img.src = IMAGE_LIST[idx];
+    cnt.textContent = `${idx + 1} / ${IMAGE_LIST.length}`;
+  });
+
+  /* ---------- 5) プリロード（任意） ---------- */
+  IMAGE_LIST.slice(1).forEach(src => { const b = new Image(); b.src = src; });
+})();
